aee5c88ab6f1d0c56720665c967a82c9d263af2f
sunkachun <sunkachun1999825@gmail.com>
2025-09-23 17:01:34 +0800

    first commit with clean architecture structure and basic library

---
+++ app/src/main/java/com/example/scmptechnicaltest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.scmptechinicaltest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.scmptechinicaltest.R
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
---
+++ app/src/main/java/com/example/scmptechnicaltest/login/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.scmptechnicaltest.login
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+
+}
